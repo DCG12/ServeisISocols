7fde422173e9ebc672576be2dd63adafcca4cbde
David <46406163y@iespoblenou.org>
2017-04-09 20:09:00 +0200

    commit v.5

--- src/com/company/SocketServidor.java
+++ src/com/company/SocketServidor.java
@@ -1,17 +1,17 @@
 package com.company;
 
-import java.io.IOException;
+import jdk.nashorn.internal.runtime.Debug;
+
+import java.io.*;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.channels.SocketChannel;
+import java.util.StringTokenizer;
 
 public class SocketServidor {
 
-
-    // https://www.adictosaltrabajo.com/tutoriales/webserver/
-
-
-    
     public static void main(String[] args) {
 
         try{
@@ -21,7 +21,7 @@ public class SocketServidor {
 
             System.out.println("realizando el bind (.3vinculo)");
 
-            InetSocketAddress addr = new InetSocketAddress("localhost", 5125);
+            InetSocketAddress addr = new InetSocketAddress("localhost", 9090);
             serverSocket.bind(addr);
 
             System.out.println("aceptando conexiones");
@@ -33,14 +33,70 @@ public class SocketServidor {
                 HiloPeticion hp = new HiloPeticion(newsocket);
                 hp.start();
                 System.out.println("Conexion recibida");
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        newsocket.getInputStream()));
+                PrintWriter out = new PrintWriter(newsocket.getOutputStream());
+
+                String str = ".";
+                while (!str.equals("")) {
+                    str = in.readLine();
+
+                    if(str.contains("GET")){
+
+                    out.println("HTTP/1.0 200 OK");
+                    out.println("Content-Type: text/html");
+                    out.println("Server: Bot");
+
+                    out.println("");
+                    InetAddress id = newsocket.getInetAddress();
+                    int aux = newsocket.getSoLinger();
+                    System.out.println("LOG: " + str);
+                    out.println("<!DOCTYPE html>\n" +
+                            "<html lang=\"en\">\n" +
+                            "<head>\n" +
+                            "<meta charset=\"UTF-8\">\n" +
+                            "<title>Servidor David Casas</title>\n" +
+                            "<h2>IP: " + id + " </h2>\n" +
+                            "<b>Esto es un servidor WEB!</b>\n" +
+                            "</head>\n" +
+                            "<body>\n" +
+                            "</body>\n" +
+                            "</html>");
+                    out.flush();
+                    newsocket.close();}
+                    else {
+                        out.println("HTTP/1.0 200 OK");
+                        out.println("Content-Type: text/html");
+                        out.println("Server: Bot");
+
+                        out.println("");
+                        InetAddress id = newsocket.getInetAddress();
+                        int aux = newsocket.getSoLinger();
+                        System.out.println("Aux: " + str);
+                        out.println("<!DOCTYPE html>\n" +
+                                "<html lang=\"en\">\n" +
+                                "<head>\n" +
+                                "<meta charset=\"UTF-8\">\n" +
+                                "<title>Servidor David Casas</title>\n" +
+                                "<b>La petición no es GET, porfavor vuelva a intentarlo.</b>\n" +
+                                "</head>\n" +
+                                "<body>\n" +
+                                "</body>\n" +
+                                "</html>");
+                        out.flush();
+                        newsocket.close();
+                    }
+                }
             }
-            //serverSocket.close();
+            } catch (IOException e1) {
+          e1.printStackTrace();
+        }
+
 
         }
-        catch(IOException e){
-            e.printStackTrace();
+
         }
-    }
 
 
-}
+
+
